6ce19eaffb7cd7cf17978bc038a4329b2d335bcf
Sean Kelly <kelly@seankelly.biz>
2020-10-07 20:26:59 -0500

    Another stab at maven workage

--- README.md
+++ README.md
@@ -109,3 +109,24 @@ Or run it locally:
 
     env ADMIN_GITHUB_TOKEN=abcd0123 GITHUB_REPOSITORY=owner/repo GITHUB_WORKSPACE=/tmp PATH=${PWD}/bin:${PATH} ${PWD}/bin/roundup --debug --assembly unstable
 
+
+### 🤷‍♀️ Buildout
+
+For reasons I can't fathom, the Python environment used to bootstrap the [buildout](http://buildout.org/) must have [github3.py](https://pypi.org/project/github3.py/) installed into it, despite `github3.py` listed as a dependency in this package. So, as of 2020-10-05, here are the gymnastics:
+
+```console
+python3 -m venv /tmp/huh
+/tmp/huh/bin/pip install github3.py
+/tmp/huh/bin/python3 bootstrap.py --setuptools-version=50.3.0
+bin/buildout
+```
+
+You can then:
+
+- Run `bin/python` to get a Python with dependencies "baked in" for testing or exploration
+- Run `bin/roundup` to try a local roundup (but be prepared to pass in an insane amount of environment variables; see above)
+- Run `bin/test` to execute unit, functional, and integration tests with XML test reports suitable for Jenkins or other CI/CD tools
+- Expore `parts/omelette` for a greppable source tree of the Roundup code and all its dependencies
+
+
+
--- src/pds/roundup/_maven.py
+++ src/pds/roundup/_maven.py
@@ -3,33 +3,63 @@
 '''🤠 PDS Roundup: Maven context'''
 
 from . import Context
-from .step import Step, StepName, NullStep, ChangeLogStep
-import logging
+from .errors import InvokedProcessError
+from .step import Step, StepName, NullStep, ChangeLogStep, DocPublicationStep, RequirementsStep
+from .util import invoke, invokeGIT
+from lxml import etree
+import logging, os
 
 _logger = logging.getLogger(__name__)
+
+_mavenNamespace = 'http://maven.apache.org/POM/4.0.0'
 
 
 class MavenContext(Context):
     '''A Maven context supports Maven (Java) software proejcts'''
-    def __init__(self, name, cwd, environ):
+    def __init__(self, cwd, environ):
         self.steps = {
             StepName.null:                NullStep,
             StepName.unitTest:            _UnitTestStep,
             StepName.integrationTest:     _IntegrationTestStep,
             StepName.changeLog:           ChangeLogStep,
-            StepName.requirements:        _RequirementsStep,
+            StepName.requirements:        RequirementsStep,
             StepName.docs:                _DocsStep,
             StepName.build:               _BuildStep,
             StepName.githubRelease:       _GitHubReleaseStep,
             StepName.artifactPublication: _ArtifactPublicationStep,
             StepName.docPublication:      _DocPublicationStep,
         }
-        super(MavenContext, self).__init__(name, cwd, environ)
+        super(MavenContext, self).__init__(cwd, environ)
+
+    def getVersionFromPOM(self):
+        '''Get the version string from a ``pom.xml`` file'''
+        cwd = self.assembly.context.cwd
+        pomFile = os.path.join(cwd, 'pom.xml')
+        if not os.path.isfile(pomFile):
+            _logger.info('☡ No ``pom.xml`` found in %s; cannot determine version', cwd)
+            return None
+        pom = etree.parse(pomFile)
+        versions = pom.xpath('/ns:project/ns:version', namespaces={'ns': _mavenNamespace})
+        if len(versions) == 0:
+            _logger.info('☡ No ``<version>`` found in %s; cannot determine version', pomFile)
+            return None
+        elif len(versions) > 1:
+            _logger.info(
+                '☡ More than one ``<version>`` found in %s; using the first one, but your POM is bad',
+                pomFile
+            )
+        return versions[0].text.strip()
 
 
-class _UnitTestStep(Step):
+class _MavenStep(Step):
+    '''☕️ Maven steps provide common conveniences for Maven and the Java environment'''
+    pass
+
+
+class _UnitTestStep(_MavenStep):
     def execute(self):
         _logger.debug('Maven unit test step')
+        invoke(['mvn', 'test'])
 
 
 class _IntegrationTestStep(Step):
@@ -37,31 +67,57 @@ class _IntegrationTestStep(Step):
         _logger.debug('Maven integration test step; TBD')
 
 
-class _RequirementsStep(Step):
-    def execute(self):
-        _logger.debug('Maven requierments step; TBD')
-
-
 class _DocsStep(Step):
     def execute(self):
-        _logger.debug('Maven docs step; TBD')
+        _logger.debug('Maven docs step')
+        invoke(['mvn', 'site'])
 
 
 class _BuildStep(Step):
     def execute(self):
-        _logger.debug('Maven build step; TBD')
+        _logger.debug('Maven build step')
+        invoke(['mvn', 'compile'])
 
 
 class _GitHubReleaseStep(Step):
     def execute(self):
-        _logger.debug('Maven GitHub release step; TBD')
+        _logger.debug('Maven GitHub release step')
+
+        token = self.getToken()
+        if not token:
+            _logger.info('🤷‍♀️ No GitHub administrative token; cannot release to GitHub')
+            return
+
+        # 😮 TODO: Use Python GitHub API!
+        # 🤷‍♀️ Thomas uses ``--unshallow``, but when I try that I get an error.
+        # So we skip it for now:
+        invokeGIT(['fetch', '--prune', '--tags'])
+        tags = invokeGIT(['tag', '--list', '*dev*'])
+        for tag in tags:
+            tag = tag.strip()
+            try:
+                invokeGIT(['tag', '--delete', tag])
+                invokeGIT(['push', '--delete', 'origin', tag])
+            except InvokedProcessError:
+                pass
+        invoke(['maven-snapshot-release', '--token', token])
 
 
 class _ArtifactPublicationStep(Step):
     def execute(self):
         _logger.debug('Maven artifact publication step; TBD')
+        if self.assembly.isStable():
+            invoke(['mvn', '-DremoveSnapshot=true', 'versions:set'])
+            invokeGIT(['add', 'pom.xml'])
+            version = self.getVersionFromPOM()
+            invoke(['mvn', '--activate-profiles', 'release', 'clean', 'site', 'deploy'])
+            invokeGIT(['git', 'tag', 'v' + version])
+            invokeGIT(['git', 'push', '--tags'])
+        else:
+            version = self.getVersionFromPOM()
+            invoke(['mvn', 'clean', 'site', 'deploy'])
 
 
-class _DocPublicationStep(Step):
-    def execute(self):
-        _logger.debug('Maven doc publication step; TBD')
+class _DocPublicationStep(DocPublicationStep):
+    def getDocDir(self):
+        return 'target/site'
